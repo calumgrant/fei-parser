f1e6bfe3f20c926ba7b51bfc4df0f2127fb0a345
Calum Grant <calumgrant@github.com>
2021-09-10 07:31:31 +0100

    JSON lexer and parser

--- parsers/json/jsonlexer.cpp
+++ parsers/json/jsonlexer.cpp
@@ -3,11 +3,31 @@
 
 using namespace cellar;
 
-using False = string<'f', 'a', 'l', 's', 'e'>;
-using True = string<'t', 'r', 'u', 'e'>;
+using Int = alt<ch<'0'>, seq<chrange<'1', '9'>, star<digit>>>;
 
+using Frac = seq<ch<'.'>, plus<digit>>;
+
+using Exp = seq<chalt<'e', 'E'>, optional<chalt<'+','-'>>, plus<digit>>;
+
+using Number = seq<optional<ch<'-'>>, Int, optional<Frac>, optional<Exp>>;
+
+using unescaped = alt<chrange<0x20, 0x21>, chrange<0x23, 0x5b>, chrange<0x5d, 0x10ffff>>;
+
+using HEX = alt<digit, chrange<'A', 'F'>, chrange<'a', 'f'>>;
+using StringChar = alt<unescaped, seq<ch<'\\'>, alt<chalt<'\"', '\\', '/', 'b', 'f', 'r', 'n', 't'>, seq<ch<'u'>, HEX, HEX, HEX, HEX>>>>;
+using String = seq<ch<'\"'>, star<StringChar>, ch<'\"'>>;
 
 using Tokens = alt <
-    token<json::True, True>
+    token<'[', ch<'['>>,
+    token<']', ch<']'>>,
+    token<'{', ch<'{'>>,
+    token<'}', ch<'}'>>,
+    token<':', ch<':'>>,
+    token<',', ch<','>>,
+    token<json::True, string<'t', 'r', 'u', 'e'>>,
+    token<json::Null, string<'n', 'u', 'l', 'l'>>,
+    token<json::False, string<'f', 'a', 'l', 's', 'e'>>,
+    token<json::Number, Number>,
+    token<json::String, String>
     >;
 char_lexer json::lexer = make_lexer<Tokens>();
--- tests/testJson.cpp
+++ tests/testJson.cpp
@@ -1,11 +1,13 @@
 #include <simpletest.hpp>
 #include <cellar/json.hpp>
+#include <cellar/output.hpp>
 
 int main()
 {
-    auto t = cellar::json::parser.parse("123");
+    auto t = cellar::json::parser.parse("[\"123\"]");
     if(t)
     {
         std::cout << "Parse success!\n";
+        std::cout << t;
     }
 }
