3063888dba87d6551e8a253983e04fb2dd3a169e
Calum Grant <calumgrant@github.com>
2021-09-15 13:50:05 +0100

    Working on type tracing

--- include/cellar/impl/first.hpp
+++ include/cellar/impl/first.hpp
@@ -13,18 +13,30 @@ namespace cellar
     {
         // Base case is to look at the "rules" member of a user-defined type.
         using type = typename first<typename Symbol::rules, typename typeset_insert<Symbol, Visited>::type>::type;
+
+        using profile_tag = first_tag;
+        using profile_types = profile_types<
+            typeset_insert<Symbol, Visited>, 
+            first<typename Symbol::rules, typename typeset_insert<Symbol, Visited>::type>
+            >;
     };
 
     template<typename Symbol, typename Visited>
     struct first<Symbol, Visited, true>
     {
         using type = typeset<>;
+
+        using profile_tag = first_tag;
+        using profile_types = profile_types<typeset_contains<Symbol, Visited>>;
     };
 
     template<typename Visited>
     struct first<symbol<>, Visited, false>
     {
         using type = typeset<>;
+
+        using profile_tag = first_tag;
+        using profile_types = profile_types<symbol<>, typeset_contains<symbol<>, Visited>>;
     };
 
     template<typename Rule, typename... Rules, typename Visited>
@@ -33,18 +45,39 @@ namespace cellar
         using S1 = typename first<Rule, Visited>::type;
         using S2 = typename first<symbol<Rules...>, Visited>::type;
         using type = typename typeset_sorted_union<S1,S2>::type;
+
+        using profile_tag = first_tag;
+        using profile_types = profile_types<
+            typeset_contains<symbol<Rule, Rules...>, Visited>,
+            first<Rule, Visited>,
+            first<symbol<Rules...>, Visited>,
+            typeset_sorted_union<S1,S2>,
+            type
+            >;
     };
 
     template<int Id, typename Visited>
     struct first<rule<Id>, Visited, false>
     {
-        using type = typeset<>;        
+        using type = typeset<>;    
+        
+        using profile_tag = first_tag;
+        using profile_types = profile_types<
+            typeset_contains<rule<Id>, Visited>,
+            type
+            >;
     };
 
     template<int Id, typename...Def, typename Visited>
     struct first<token<Id, Def...>, Visited, false>
     {
         using type = typeset<token<Id>>;
+
+        using profile_tag = first_tag;
+        using profile_types = profile_types<
+            typeset_contains<token<Id, Def...>, Visited>,
+            type
+            >;
     };
 
     template<int Id, typename Symbol, typename...Symbols, typename Visited>
@@ -54,5 +87,15 @@ namespace cellar
         using S2 = typename first<rule<Id, Symbols...>, Visited>::type;
         using U = typename typeset_sorted_union<S1,S2>::type;
         using type = typename type_if<potentially_empty_symbol<Symbol>::value, U, S1>::type;
+
+        using profile_tag = first_tag;
+        using profile_types = profile_types<
+            typeset_contains<rule<Id, Symbol, Symbols...>, Visited>,
+            type,
+            first<Symbol, Visited>,
+            first<rule<Id, Symbols...>, Visited>,
+            typeset_sorted_union<S1,S2>,
+            potentially_empty_symbol<Symbol>
+            >;
     };
 }
--- include/cellar/impl/potentially_empty_symbol.hpp
+++ include/cellar/impl/potentially_empty_symbol.hpp
@@ -10,6 +10,13 @@ namespace cellar
     struct potentially_empty_symbol
     {
         static const bool value = potentially_empty_symbol<typename Symbol::rules, typename typeset_insert<Symbol, Visited>::type>::value;
+        using profile_tag = potentially_empty_symbol_tag;
+        using profile_types = profile_types<
+            Symbol,
+            Visited,
+            typeset_contains<Symbol, Visited>, typeset_insert<Symbol, Visited>, 
+            potentially_empty_symbol<typename Symbol::rules, typename typeset_insert<Symbol, Visited>::type>
+            >;
     };
 
     template<typename Symbol, typename Visited>
@@ -17,30 +24,68 @@ namespace cellar
     {
         // Recursive case
         static const bool value = false;
+        using profile_tag = potentially_empty_symbol_tag;
+        using profile_types = profile_types<
+            Symbol,
+            Visited,
+            typeset_contains<Symbol, Visited>
+            >; 
     };
 
     template<typename Visited>
     struct potentially_empty_symbol<symbol<>, Visited, false>
     {
         static const bool value = false;
+
+        using profile_tag = potentially_empty_symbol_tag;
+        using profile_types = profile_types<
+            symbol<>,
+            Visited,
+            typeset_contains<symbol<>, Visited>
+            >; 
     };
 
     template<typename Rule, typename...Rules, typename Visited>
     struct potentially_empty_symbol<symbol<Rule, Rules...>, Visited, false>
     {
         static const bool value = potentially_empty_symbol<Rule, Visited>::value || potentially_empty_symbol<symbol<Rules...>, Visited>::value;
+
+        using profile_tag = potentially_empty_symbol_tag;
+        using profile_types = profile_types<
+            symbol<Rule, Rules...>,
+            Visited,
+            typeset_contains<symbol<Rule, Rules...>, Visited>,
+            potentially_empty_symbol<Rule, Visited>,
+            potentially_empty_symbol<symbol<Rules...>, Visited>            
+            >; 
+
     };
 
     template<int Id, typename...Definition, typename Visited>
     struct potentially_empty_symbol<token<Id, Definition...>, Visited, false>
     {
         static const bool value = false;
+
+        using profile_tag = potentially_empty_symbol_tag;
+        using profile_types = profile_types<
+            token<Id, Definition...>,
+            Visited,
+            typeset_contains<token<Id, Definition...>, Visited>
+            >;
+
     };
 
     template<int Id, typename Visited>
     struct potentially_empty_symbol<rule<Id>, Visited, false>
     {
         static const bool value = true;
+
+        using profile_tag = potentially_empty_symbol_tag;
+        using profile_types = profile_types<
+            rule<Id>,
+            Visited,
+            typeset_contains<rule<Id>, Visited>
+            >;
     };
 
     template<int Id, typename Rule, typename...Rules, typename Visited>
@@ -48,5 +93,14 @@ namespace cellar
     {
         static const bool value = potentially_empty_symbol<Rule, Visited>::value &&
             potentially_empty_symbol<rule<Id, Rules...>, Visited>::value;
+
+        using profile_tag = potentially_empty_symbol_tag;
+        using profile_types = profile_types<
+            rule<Id>,
+            Visited,
+            typeset_contains<rule<Id, Rule, Rules...>, Visited>,
+            potentially_empty_symbol<Rule, Visited>,
+            potentially_empty_symbol<rule<Id, Rules...>, Visited>
+            >;
     };    
 }
